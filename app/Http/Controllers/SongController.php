ea4bedb2bd6c346b7397c654fd82713283029e73
feremerz <feremerz@gmail.com>
2019-07-04 12:11:07 +0430

    song categorizable

---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Song;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class SongController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $song=Song::create([
+           'name'=>'Amir Tataloo - Khar',
+           'release_date'=>Carbon::now(),
+           'lyrics'=>'بازم خاااره',
+           'likeCount'=>5,
+           'viewCount'=>10,
+           'status'=>1,
+           'duration'=>1232,
+           'slug'=>'amir-tataloo-khar',
+           'album_id'=>1
+        ]);
+        $category=Category::find(1);
+        $song->categories()->save($category);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
